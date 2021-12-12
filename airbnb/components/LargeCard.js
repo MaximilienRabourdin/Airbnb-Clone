fd8fc9fa09981cef69a9e7703026413d5f7ef82f
MaximilienRabourdin <maximilien.rabourdin@gmail.com>
2021-12-12 23:00:02 +0100

    static components

--- airbnb/components/LargeCard.js
+++ airbnb/components/LargeCard.js
@@ -12,7 +12,7 @@ function LargeCard({ img,title, description, buttonText }) {
         />
       </div>
 
-      <div className="absolute top-32 left-12 ">
+      <div className="absolute top-32 left-10 ">
         <h3 className="text-white text-4xl nb-3 w-64">{title}</h3>
         <p className="text-red-500">{description}</p>
         <button className=" text-sm text-white bg-gray-900 px-2 py-2 rounded-lg mt-5">{buttonText}</button>
