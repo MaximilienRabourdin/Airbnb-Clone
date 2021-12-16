f1dea4518be74c45d3b88d7e84ec4a92eac2fbd5
Maximilien Rabourdin <maximilien.rabourdin@gmail.com>
2021-12-16 16:40:12 +0100

    infoCard Component finish

--- pages/search.js
+++ pages/search.js
@@ -1,74 +1,83 @@
-import Header from "../components/Header"
-import Footer from "../components/Footer"
-import { useRouter } from "next/router"
-import {parseISO, format} from 'date-fns/format'
+import { useRouter } from "next/dist/client/router";
+import Header from "../components/Header";
+import InfoCard from "../components/InfoCard";
 
+import { format } from "date-fns";
+import dynamic from "next/dynamic";
+import Footer from "../components/Footer";
 
-function Search({searchResults}) {
+// // Client Side Render as we need Global Window Object
+// const Map = dynamic(() => import("../components/Map"), {
+//   loading: () => "Loading...",
+//   ssr: false,
+// });
 
-    console.log(searchResults);
+function Search({ searchResults }) {
+  const router = useRouter();
+  const { location, startDate, endDate, noOfGuests } = router.query;
 
-    // ES6 desctucturing
-    const router = useRouter();
-    const { location, startDate, endDate, noOfGuests} = router.query;
+//   const formattedStartDate = format(new Date(startDate), "dd MMMM yy");
+//   const formattedEndDate = format(new Date(endDate), "dd MMMM yy");
+//   const range = `${formattedStartDate} - ${formattedEndDate}`;
 
+  return (
+    <div className="h-screen">
+      <Header placeholder={`${location} | ${noOfGuests} guests`} />
 
-    const {format} = require('date-fns');
-    // const formattedStartDate = format(new Date(startDate), "ddMMMMyy");
-    // const formattedEndDate = format(new Date(endDate), "ddMMMMyy");
-    // const range = `${formattedStartDate} - ${formattedEndDate}`;
+      <main className="flex">
+        <section className="flex-grow pt-14 px-6">
+          <p className="text-xs">
+            300+ stays - {noOfGuests} guests{" "}
+          </p>
 
+          <h1 className="text-3xl font-semibold mt-2 mb-6">
+            Stays in {location}
+          </h1>
 
-    return (
-        <div>
-           <Header placeholder={`${location} | ${noOfGuests}`} />
+          <div className="hidden lg:inline-flex mb-5 space-x-3 text-gray-800 whitespace-nowrap">
+            <p className="button">Cancellation Flexibility</p>
+            <p className="button">Type of place</p>
+            <p className="button">Price</p>
+            <p className="button">Rooms and Beds</p>
+            <p className="button">More filters</p>
+          </div>
 
-           <main className="flex">
-            <section className="flex-grow pt-14 px-6">
-                <p className="text-xs mt-2">+300 reservation for {noOfGuests} membres des visiteurs</p>
-                <h1 className="text-3xl font-semibold mt-2 mb-6"> Rester dans le meilleur endroit : {location} </h1>
+          <div className="flex flex-col">
+            {searchResults.map(
+              ({ img, location, title, description, star, price, total }) => (
+                <InfoCard
+                  key={img}
+                  img={img}
+                  location={location}
+                  title={title}
+                  description={description}
+                  star={star}
+                  price={price}
+                  total={total}
+                />
+              )
+            )}
+          </div>
+        </section>
 
-                <div className="hidden lg:inline-flex justify-between mb-5 space-x-
-            3 text-gray-800 whitespace-nowrap">
-                    <p className="button"> Annulation de la réservation  </p>
-                    <p className="button"> Avis </p>
-                    <p className="button"> Type de logement </p>
-                    <p className="button"> Nombre de chambres </p>
-                    <p className="button"> Plus de filtres </p>
-                </div>
+    
+      </main>
 
-                
-                <div>       
-                {searchResults.map(
-                    ({img, location, title, description, star, price, total}) => (
-                    <InfoCard
-                    key={img}
-                    img={img}
-                    location={location}
-                    title={title}
-                    description={description}
-                    star={star}
-                    price={price}
-                    total={total} />
-                )
-                )}
-                </div>
-            </section>
-           </main>
-
-           <Footer />
-        </div>
-    )
+      <Footer />
+    </div>
+  );
 }
 
-export default Search
+export default Search;
 
-export async function getServeSideProps() {
-    const searchResults = await fetch ("https://links.papareact.com/isz").then(res => res.json());
+export async function getServerSideProps() {
+  const searchResults = await fetch("https://links.papareact.com/isz").then(
+    (res) => res.json()
+  );
 
-    return {
-        props:{
-            searchResults,
-        } ,
-    };
+  return {
+    props: {
+      searchResults,
+    },
+  };
 }
